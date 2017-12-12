07e810a0fe00a0855651966ce5853d8c72da64c3
LHills <leahhills@gmail.com>
2017-12-12 12:34:35 -0700

    Adding styling to Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Header from './../Header/Header';
+import './../Header/Header.css';
+import './Profile.css';
+import dogpic from './../../pictures/dogpic.jpg';
 
 export default class Profile extends Component {
     
@@ -7,7 +10,28 @@ export default class Profile extends Component {
         return (
           <div>
               <Header/>
+                <div className="profile_parent">
 
+                    <div className="profile_child">
+                        
+                        <div className="user_content_header">
+                            <img src={ dogpic } alt="supposed to be a robot pic"className="profile_img"/>
+                            <button className="update_button">Update</button>
+                            <button className="cancel_button">Cancel</button>
+                        </div>
+
+                        <div className="user_select_options">
+                        </div>
+                    
+                    </div>
+
+
+
+
+
+
+
+                </div>
               Gueeess what i'm the profile page.
           </div>  
         );
