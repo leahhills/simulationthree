e6e7c6cc30736c973e67678f5303b2c1c95d7c00
LHills <leahhills@gmail.com>
2018-01-05 00:14:37 -0700

    finished endpoint for updating user

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -3,15 +3,51 @@ module.exports={
         const dbInstance = req.app.get('db');
         const {firstname,lastname,gender,haircolor,eyecolor,hobby,birthday,birthmonth,birthyear} = req.body;
      console.log(req.params.id,firstname,lastname,gender,haircolor,eyecolor,hobby,birthday,birthmonth,birthyear);
-    dbInstance.updateUser([req.params.id,firstname,lastname,gender,haircolor,eyecolor,hobby,birthday,birthmonth,birthyear])
-    .then(response=>{
-        console.log('what response is',response);
-        res.status(200).send('Yah we got yu updated');
-    })
-    .catch(err=>{
-        console.log('update error',err);
-        res.status(500).send(err);
-    })
+
+    dbInstance.find_user([req.params.id])
+    .then(response => {
+        let currentUser = response[0];
+        console.log('Current User Before Change', currentUser);
+        if(firstname)
+            currentUser.firstname = firstname;
+        if(lastname)
+            currentUser.lastname = lastname;
+        if(gender)
+            currentUser.gender = gender;
+        if(haircolor)
+            currentUser.haircolor = haircolor;
+        if(eyecolor)
+            currentUser.eyecolor = eyecolor;
+        if(hobby)
+            currentUser.hobby = hobby;
+        if(birthday)
+            currentUser.birthday = birthday;
+        if(birthmonth)
+            currentUser.birthmonth = birthmonth;
+        if(birthyear)
+            currentUser.birthyear = birthyear;
+        console.log('Current User AFTER Change', currentUser);
+            
+        dbInstance.update_user([req.params.id, currentUser.firstname, currentUser.lastname, currentUser.gender, currentUser.haircolor, currentUser.eyecolor, currentUser.hobby, currentUser.birthday, currentUser.birthmonth, currentUser.birthyear])
+        .then(response => {
+            console.log('response from update', response)
+            res.status(200).send('yah we got updated');
+        })
+        .catch(err => {
+            console.log('error updating', err);
+            res.status(500).send(err);
+        });
+    });
+
+    // dbInstance.updateUser([req.params.id,firstname,lastname,gender,haircolor,eyecolor,hobby,birthday,birthmonth,birthyear])
+    // .then(response=>{
+    //     console.log('what response is',req.body);
+    //     res.status(200).send('Yah we got yu updated');
+    // })
+    // .catch(err=>{
+    //     console.log('update error',err);
+    //     res.status(500).send(err);
+    // })
 
     }
 
