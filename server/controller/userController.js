4cb7d727c5fe7d96f273e0d834ad069661736c75
LHills <leahhills@gmail.com>
2018-01-06 20:17:23 -0700

    finished get user list endpoint

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,74 +1,85 @@
-module.exports={
-    updateQualities:(req,res,next)=>{
+module.exports = {
+    updateQualities: (req, res, next) => {
         const dbInstance = req.app.get('db');
-        const {firstname,lastname,gender,haircolor,eyecolor,hobby,birthday,birthmonth,birthyear} = req.body;
-     console.log(req.params.id,firstname,lastname,gender,haircolor,eyecolor,hobby,birthday,birthmonth,birthyear);
+        const {
+            firstname,
+            lastname,
+            gender,
+            haircolor,
+            eyecolor,
+            hobby,
+            birthday,
+            birthmonth,
+            birthyear
+        } = req.body;
+        console.log(req.params.id, firstname, lastname, gender, haircolor, eyecolor, hobby, birthday, birthmonth, birthyear);
 
-    dbInstance.find_userfromlist([req.params.id])
-    .then(response => {
-        let currentUser = response[0];
-        console.log('Current User Before Change', currentUser);
-        if(firstname)
-            currentUser.firstname = firstname;
-        if(lastname)
-            currentUser.lastname = lastname;
-        if(gender)
-            currentUser.gender = gender;
-        if(haircolor)
-            currentUser.haircolor = haircolor;
-        if(eyecolor)
-            currentUser.eyecolor = eyecolor;
-        if(hobby)
-            currentUser.hobby = hobby;
-        if(birthday)
-            currentUser.birthday = birthday;
-        if(birthmonth)
-            currentUser.birthmonth = birthmonth;
-        if(birthyear)
-            currentUser.birthyear = birthyear;
-        console.log('Current User AFTER Change', currentUser);
-            
-        dbInstance.update_user([req.params.id, currentUser.firstname, currentUser.lastname, currentUser.gender, currentUser.haircolor, currentUser.eyecolor, currentUser.hobby, currentUser.birthday, currentUser.birthmonth, currentUser.birthyear])
-        .then(response => {
-            console.log('response from update', response)
-            res.status(200).send('yah we got updated');
-        })
-        .catch(err => {
-            console.log('error updating', err);
-            res.status(500).send(err);
-        });
-    });
+        dbInstance.find_userfromlist([req.params.id])
+            .then(response => {
+                let currentUser = response[0];
+                console.log('Current User Before Change', currentUser);
+                if (firstname)
+                    currentUser.firstname = firstname;
+                if (lastname)
+                    currentUser.lastname = lastname;
+                if (gender)
+                    currentUser.gender = gender;
+                if (haircolor)
+                    currentUser.haircolor = haircolor;
+                if (eyecolor)
+                    currentUser.eyecolor = eyecolor;
+                if (hobby)
+                    currentUser.hobby = hobby;
+                if (birthday)
+                    currentUser.birthday = birthday;
+                if (birthmonth)
+                    currentUser.birthmonth = birthmonth;
+                if (birthyear)
+                    currentUser.birthyear = birthyear;
+                console.log('Current User AFTER Change', currentUser);
 
-    // dbInstance.updateUser([req.params.id,firstname,lastname,gender,haircolor,eyecolor,hobby,birthday,birthmonth,birthyear])
-    // .then(response=>{
-    //     console.log('what response is',req.body);
-    //     res.status(200).send('Yah we got yu updated');
-    // })
-    // .catch(err=>{
-    //     console.log('update error',err);
-    //     res.status(500).send(err);
-    // })
+                dbInstance.update_user([req.params.id, currentUser.firstname, currentUser.lastname, currentUser.gender, currentUser.haircolor, currentUser.eyecolor, currentUser.hobby, currentUser.birthday, currentUser.birthmonth, currentUser.birthyear])
+                    .then(response => {
+                        console.log('response from update', response)
+                        res.status(200).send('yah we got updated');
+                    })
+                    .catch(err => {
+                        console.log('error updating', err);
+                        res.status(500).send(err);
+                    });
+            });
+
+        // dbInstance.updateUser([req.params.id,firstname,lastname,gender,haircolor,eyecolor,hobby,birthday,birthmonth,birthyear])
+        // .then(response=>{
+        //     console.log('what response is',req.body);
+        //     res.status(200).send('Yah we got yu updated');
+        // })
+        // .catch(err=>{
+        //     console.log('update error',err);
+        //     res.status(500).send(err);
+        // })
 
     },
 
-        getUserList:(req,res,next)=>{
-            const dbInstance = req.app.get('db');
-            const { id }= req.body;
-            dbInstance.read_userslist([req.params.id,id])
-            .then(response => {
-                // if(id<=24)
-                //     return read_userslist;
-                // if
-                    
-
-
-                console.log(response);
-                res.status(200).send(response);
+    getUserList: (req, res, next) => {
+        const dbInstance = req.app.get('db');
+        const page = req.query.page;
+        let offset = (page - 1) * 24;
+        dbInstance.read_userlist_paginate([offset])
+            .then(users => {
+                dbInstance.userlist_count([])
+                .then(userCount => {
+                    let numPages = Math.ceil(userCount[0].count / 24);
+                    let response = {
+                        userList: users,
+                        userCount: userCount[0].count,
+                        totalPages: numPages
+                    }
+                    res.status(200).send(response);
+                });
             })
-            .catch(err=> res.status(500).send(err));
-            
-
-},
-    
-
+            .catch(err => {
+                res.status(500).send(err);
+            });
+    },
 }
